4c00771ea2faee5dc7cc1b34e2ba0ce8d58d7e88
Remo Rothacher <rro@nexplore.ch>
2026-01-26 20:59:07 +0100

    Revert "undo if checks"
    This reverts commit 776b7ccaa23b9beb99cd95649edcf08dca39f2c9.

--- src/renderer/api/jellyfin/jellyfin-api.ts
+++ src/renderer/api/jellyfin/jellyfin-api.ts
@@ -469,7 +469,7 @@ export const jfApiClient = (args: {
                     return {
                         body: response?.data,
                         headers: response?.headers as any,
-                        status: response?.status,
+                        status: response?.status || 0,
                     };
                 }
                 throw e;
