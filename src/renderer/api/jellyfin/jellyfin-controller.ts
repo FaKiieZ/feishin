7c9477e737356da171aad615c2fe64e270e94452
Remo Rothacher <rro@nexplore.ch>
2026-01-26 20:44:31 +0100

    undo ifs

--- src/renderer/api/jellyfin/jellyfin-controller.ts
+++ src/renderer/api/jellyfin/jellyfin-controller.ts
@@ -247,12 +247,7 @@ export const JellyfinController: InternalControllerEndpoint = {
             }),
         ]);
 
-        if (
-            !res?.status ||
-            res.status !== 200 ||
-            !similarArtistsRes?.status ||
-            similarArtistsRes.status !== 200
-        ) {
+        if (res.status !== 200 || similarArtistsRes.status !== 200) {
             throw new Error('Failed to get album artist detail');
         }
 
@@ -325,7 +320,7 @@ export const JellyfinController: InternalControllerEndpoint = {
             },
         });
 
-        if (res.status !== 200 || !songsRes?.status || songsRes.status !== 200) {
+        if (res.status !== 200 || songsRes.status !== 200) {
             throw new Error('Failed to get album detail');
         }
 
